d2741ce256638d5f1c66c1d87baef48a9dbae4c6
Kofi Sarfo <kofisarfo@hotmail.com>
2018-11-17 17:13:46 -0400

    add winning conditions

--- Kofi.Waves/Kofi.Waves/Player.cs
+++ Kofi.Waves/Kofi.Waves/Player.cs
@@ -1,6 +1,38 @@
-﻿namespace Kofi.Waves
+﻿using System;
+
+namespace Kofi.Waves
 {
     public class Player
     {
+        public const char Us = 'o';
+        public const char Them = 'x';
+        public const char Neither = ' ';
+    }
+
+    public class Board
+    {
+        private char[] _validPieces = new char[] { Player.Us, Player.Them, Player.Neither };
+
+        public string Squares { get; }
+
+        public Board(string squares)
+        {
+            Squares = squares;
+        }
+    }
+
+    public static class Winning
+    {
+        public static int[,] States => new[,]
+        {
+            { 0, 1, 2 },
+            { 3, 4, 5 },
+            { 6, 7, 8 },
+            { 0, 3, 6 },
+            { 1, 4, 7 },
+            { 2, 5, 8 },
+            { 0, 4, 8 },
+            { 2, 4, 6 }
+        };
     }
 }
